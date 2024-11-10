5cad9d88a6c32bb7be0089f3232702a9d44fcbeb
42nick <niklashahn98@googlemail.com>
2024-11-10 19:33:53 +0100

    Initial commit

---
+++ src/gpu_monitor/main.py
@@ -0,0 +1,10 @@
+"""Just a placeholder for the main function of the project."""
+
+
+def main() -> None:
+    """Run the core function of this awesome project."""
+    print("Hello world :)")
+
+
+if __name__ == "__main__":
+    main()
---
+++ tests/test_main.py
@@ -0,0 +1,8 @@
+"""Test the main function."""
+
+from gpu_monitor.main import main
+
+
+def test_main() -> None:
+    """Test the main function."""
+    main()
